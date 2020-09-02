1a37a48c09bc686c124d2d4d77f99a3d786f01c8
Josh Mann <jlmann72@gmail.com>
2020-09-02 13:25:31 -0400

    Working solution

--- Program.cs
+++ Program.cs
@@ -2,11 +2,30 @@
 
 namespace DigitsExplosion
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        public static string Explode(string s)
+        {
+            // Given a string of digits (s), return a string
+            // where each digit is repeated a number of times equal to its value
+            var output = "";
+            for (var index = 0; index < s.Length; index++)
+            {
+                var sCharacter = s[index];
+                var sCharacterString = sCharacter.ToString();
+                var sInteger = Int32.Parse(sCharacterString);
+
+                for (var addNumber = 0; addNumber < sInteger; addNumber++)
+                {
+                    output += sCharacter;
+                }
+
+            }
+            return output;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C#");
+        }
     }
-  }
 }
